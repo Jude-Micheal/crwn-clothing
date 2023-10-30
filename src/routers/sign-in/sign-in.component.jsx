4da729b5ba450dbfe0c7d0f2a79f0df505891c88
Jude Micheal <judemicheal22@gmail.com>
2023-10-30 20:42:08 +0100

    creta

--- src/routers/sign-in/sign-in.component.jsx
+++ src/routers/sign-in/sign-in.component.jsx
@@ -3,16 +3,19 @@ import {
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
-    createUserDocumentFromAuth(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
   };
 
   return (
     <div>
       <h1>Sign in page</h1>
-      <button onClick={logGoogleUser}> SignIn with Google</button>
+      <button onClick={logGoogleUser}> SignIn with Google Popup</button>
+      <SignUpForm />
     </div>
   );
 };
